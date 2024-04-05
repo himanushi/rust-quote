5b1673cfd28a100cd2fb41ffa0000a8c8af68515
himanushi <himanushi@users.noreply.github.com>
2024-04-05 09:03:03 +0900

    macro

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,9 @@
+macro_rules! add {
+    () => { 0 };
+    ($head:expr $(; $tail:expr)*) => { $head + add!($($tail);*) };
+}
+
 fn main() {
-    println!("Hello, world!");
+    let sum = add!(1; 1; 1);
+    println!("The sum is: {}", sum);
 }
